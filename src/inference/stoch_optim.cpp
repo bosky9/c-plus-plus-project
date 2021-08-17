26bbc5187aa07a38a4c51612c6d1906bca8c51fd
Federico <fider97@gmail.com>
2021-08-17 17:28:59 +0200

    Finished implementation of stoch_optim.cpp

--- src/inference/stoch_optim.cpp
+++ src/inference/stoch_optim.cpp
@@ -23,4 +23,14 @@ ADAM::ADAM(const std::vector<double>& starting_parameters, double starting_varia
 
 std::vector<double> ADAM::update(double gradient) {
     _f_gradient = _ewma_1 * _f_gradient + (1 - _ewma_1) * gradient;
+    double f_gradient_hat = _f_gradient / (1 - pow(_ewma_1, _t));
+    _variance = _ewma_2 * _variance + (1 - _ewma_2) * pow(gradient, 2);
+    double variance_hat = _variance / (1 - pow(_ewma_2, _t));
+    if (_t > 5) {
+        double add_to_param = _learning_rate + (_learning_rate*15.0*(pow(0.99, _t))) * (f_gradient_hat/(sqrt(variance_hat)+_epsilon));
+        std::transform(_parameters.begin(), _parameters.end(), _parameters.begin(),
+                       [add_to_param](double val){return val + add_to_param});
+    }
+    _t += 1;
+    return _parameters;
 };
