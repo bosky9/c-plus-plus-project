9c4ba20596e1a8dffb76e5696bcf6b0604482489
Federico <fider97@gmail.com>
2021-08-20 11:20:05 +0200

    Added documentation

--- include/inference/metropolis_hastings.hpp
+++ include/inference/metropolis_hastings.hpp
@@ -9,14 +9,31 @@
  */
 class MetropolisHastings {
 private:
+    /// A posterior function
     std::function<double(Eigen::VectorXd)> posterior;
+
+    /// The scale for the random walk
     double scale;
+
+    /// The number of iterations to perform
     int nsims;
+
+    /// Where to start the MCMC chain
     Eigen::VectorXd initials;
+
+    /// (optional) A covariance matrix for the random walk
     Eigen::MatrixXd cov_matrix;
+
+    /// By how much to thin the chains (2 means drop every other point)
     int thinning;
+
+    /// Whether to discard first half of the chain as 'warm-up'
     bool warm_up_period;
+
+    /// A model object (for use in SPDK sampling)
     // TODO: TSM model_object;
+
+    /// Whether to print progress to console or stay quiet
     bool quiet_progress;
 public:
     MetropolisHastings(std::function<double(Eigen::VectorXd)> posterior,
@@ -31,7 +48,17 @@ public:
                        bool quiet_progress = false
     );
 
+    /**
+     * @brief Tunes scale for M-H algorithm
+     * @param acceptance The most recent acceptance rate
+     * @param scale The current scale parameter
+     * @return An adjusted scale parameter
+     */
     static double tune_scale(double acceptance, double scale);
 
+    /**
+     * @brief Sample from M-H algorithm
+     * @return A Sample object
+     */
     Sample sample();
 };
--- include/inference/sample.hpp
+++ include/inference/sample.hpp
@@ -2,9 +2,18 @@
  * @brief Sample data returned by many functions
  */
 struct Sample {
+    /// Chains for each parameter
     Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> chain;
+
+    /// Mean values for each parameter
     std::vector<double> mean_est;
+
+    /// Median values for each parameter
     std::vector<double> median_est;
+
+    /// Upper 95% credibility interval for each parameter
     std::vector<double> upper_95_est;
+
+    /// Lower 95% credibility interval for each parameter
     std::vector<double> lower_5_est;
 };
