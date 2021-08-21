640f742a7b24e3867c241c99786cfb8b7e745e1b
Federico <fider97@gmail.com>
2021-08-21 12:26:59 +0200

    Implementation of MetropolisHastings::sample()

--- src/inference/metropolis_hastings.cpp
+++ src/inference/metropolis_hastings.cpp
@@ -1,5 +1,8 @@
 #include "inference/metropolis_hastings.hpp"
 
+#include "multivariate_normal.hpp"
+#include "inference/metropolis_sampler.hpp"
+
 MetropolisHastings::MetropolisHastings(std::function<double(Eigen::VectorXd)>& posterior, double scale, int nsims,
                                        const Eigen::VectorXd& initials,
                                        const std::optional<Eigen::MatrixXd>& cov_matrix, int thinning,
@@ -31,4 +34,66 @@ double MetropolisHastings::tune_scale(double acceptance, double scale) {
     else if (acceptance <= 0.01)
         scale *= 0.1;
     return scale;
+}
+
+Sample MetropolisHastings::sample() {
+    double acceptance = 1.0;
+    bool finish = true;
+
+    while ((acceptance < 0.234 or acceptance > 0.4) or finish) {
+        // If acceptance is in range, proceed to sample, else continue tuning
+        int sims_to_do;
+        if (!(acceptance < 0.234 or acceptance > 0.4)) {
+                finish = false;
+                if (!_quiet_progress) {
+                    std::cout << "\nTuning complete! Now sampling.";
+                }
+                sims_to_do = _nsims;
+        } else {
+            sims_to_do = _nsims/2;
+        }
+        // Holds data on acceptance rates and uniform random numbers
+        Eigen::VectorXd a_rate{Eigen::VectorXd::Zero(sims_to_do)};
+        Eigen::VectorXd crit{Eigen::VectorXd::Random(sims_to_do)};
+        Mvn post{Eigen::VectorXd::Zero(_param_no), _cov_matrix};
+        Eigen::MatrixXd rnums;
+        for (size_t i{0}; i < sims_to_do; i++) {
+            rnums.row(i) = post.sample() * _scale;
+        }
+        metropolis_sampler(sims_to_do, _phi, _posterior, a_rate, rnums, crit);
+        acceptance = a_rate.sum()/a_rate.size();
+        _scale = tune_scale(acceptance, _scale);
+        if (!_quiet_progress) {
+            std::cout << "Acceptance rate of Metropolis-Hastings is " << acceptance;
+        }
+    }
+
+    // Remove warm-up and thin
+    _phi = _phi(seq(_nsims/2, last), all)(seq(0,last,_thinning), all);
+    // TODO: controlla, transpose() non deve modificare _phi (anche in norm_post_sim.hpp)
+    Eigen::MatrixXd chain = _phi.transpose();
+
+    std::vector<double> mean_est(_phi.cols());
+    Eigen::VectorXd mean_vector = _phi.colwise().mean();
+    mean_est.resize(mean_vector.size());
+    Eigen::VectorXd::Map(&mean_est[0], mean_vector.size()) = mean_vector;
+
+    std::vector<double> median_est;
+    std::vector<double> upper_95_est;
+    std::vector<double> lower_5_est;
+    for (size_t i{0}; i < _param_no; i++) {
+        std::vector<double> col_sort(_phi.rows());
+        Eigen::VectorXd::Map(&col_sort[0], NSIMS) = _phi.col(i);
+        std::sort(col_sort.begin(), col_sort.end());
+
+        if (static_cast<bool>(_phi.rows() % 2))
+            median_est.push_back(col_sort[_phi.rows() / 2]);
+        else
+            median_est.push_back((col_sort[_phi.rows() / 2] + col_sort[_phi.rows() / 2 - 1]) / 2);
+
+        upper_95_est.push_back((col_sort[_phi.rows() * 95 / 100]));
+        lower_5_est.push_back((col_sort[_phi.rows() * 5 / 100]));
+    }
+
+    return {chain, mean_est, median_est, upper_95_est, lower_5_est};
 }
