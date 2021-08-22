d956d433c617d5cfd51b10cb4cc10a4a0d0d4406
ettore <ettore.cinquetti@gmail.com>
2021-08-22 16:22:43 +0200

    Small fixes

--- include/inference/metropolis_sampler.hpp
+++ include/inference/metropolis_sampler.hpp
@@ -1,22 +1,7 @@
-#pragma once
-
+#ifndef METROPOLIS_SAMPLER
+#define METROPOLIS_SAMPLER
 #include "headers.hpp"
 
-void metropolis_sampler(int sims_to_do, Eigen::MatrixXd& phi, std::function<double(Eigen::VectorXd)> posterior,
-                        Eigen::VectorXd& a_rate, const Eigen::MatrixXd& rnums, const Eigen::VectorXd& crit) {
-
-    double old_lik = -posterior(phi.row(0)); // float in Cython
-
-    for (Eigen::Index i = 1; i < sims_to_do; i++) {
-        Eigen::VectorXd phi_prop = phi.row(i - 1) + rnums.row(i);
-        double post_prop         = -posterior(phi_prop);     // float in Cython
-        double lik_rate          = exp(post_prop - old_lik); // float in Cython
-
-        if (crit(i) < lik_rate) {
-            phi.row(i) = phi_prop;
-            a_rate(i)  = 1;
-            old_lik    = post_prop;
-        } else
-            phi(i) = phi(i - 1);
-    }
-}
+void metropolis_sampler(int sims_to_do, Eigen::MatrixXd& phi, const std::function<double(Eigen::VectorXd)>& posterior,
+                        Eigen::VectorXd& a_rate, const Eigen::MatrixXd& rnums, const Eigen::VectorXd& crit);
+#endif
--- test/inference/test_metropolis_hastings.cpp
+++ test/inference/test_metropolis_hastings.cpp
@@ -2,6 +2,9 @@
 
 #include "inference/metropolis_hastings.hpp"
 
+#include "inference/metropolis_sampler.hpp"
+
+
 TEST_CASE("New MetropolisHastings object without cov_matrix", "[MetropolisHastings]") {
     std::function<double(Eigen::VectorXd)> posterior = [](const Eigen::VectorXd& v) { return 0; };
     Eigen::VectorXd initials{Eigen::VectorXd::Zero(3)};
@@ -20,4 +23,19 @@ TEST_CASE("Tune scale", "[tune_scale]") {
     Eigen::VectorXd initials{Eigen::VectorXd::Zero(3)};
     MetropolisHastings mh = MetropolisHastings(posterior, 1.0, 3, initials);
     REQUIRE(mh.tune_scale(0.6, 1.0) == 1.3);
+}
+
+TEST_CASE("metropolis_sampler test", "[metropolis_sampler]") {
+    Eigen::MatrixXd phi{Eigen::MatrixXd::Identity(3, 3)};
+    std::function<double(Eigen::VectorXd)> posterior = [](const Eigen::VectorXd v) { return 0; };
+    Eigen::VectorXd a_rate{Eigen::VectorXd::Zero(3)};
+    Eigen::MatrixXd rnums{Eigen::MatrixXd::Identity(3, 3)};
+    Eigen::VectorXd crit{Eigen::VectorXd::Zero(3)};
+    metropolis_sampler(3, phi, posterior, a_rate, rnums, crit);
+
+    Eigen::MatrixXd phi2{Eigen::MatrixXd::Identity(3, 3)};
+    phi2.row(1) = Eigen::Vector3d(1, 1, 0);
+    phi2.row(2) = Eigen::Vector3d(1, 1, 1);
+    REQUIRE((phi - phi2).norm() == 0);
+    REQUIRE(a_rate(1) == 1);
 }
