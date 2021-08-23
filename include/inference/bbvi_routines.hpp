1ea439ec39639488980c5adece6d0d85c81315f4
ettore <ettore.cinquetti@gmail.com>
2021-08-23 14:42:35 +0200

    Added bbvi_routines.hpp

--- include/inference/bbvi_routines.hpp
+++ include/inference/bbvi_routines.hpp
@@ -1,7 +1,18 @@
 #include "headers.hpp"
 
-std::vector<double> alpha_recursion();
+std::vector<double> alpha_recursion(
+        std::vector<double>& alpha0,
+        Eigen::MatrixXd<double>& grad_log_q,
+        Eigen::MatrixXd<double>& gradient,
+        int param_no);
 
-std::vector<double> log_p_posterior();
+std::vector<double> log_p_posterior(
+        Eigen::MatrixXd<double>& z,
+        const std::function<double(Eigen::VectorXd)>& neg_posterior
+        );
 
-std::vector<double> mb_log_p_posterior();
+std::vector<double> mb_log_p_posterior(
+        Eigen::MatrixXd<double>& z,
+        const std::function<double(Eigen::VectorXd)>& neg_posterior,
+        int mini_batch;
+    );
