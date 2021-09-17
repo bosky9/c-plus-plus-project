08701a386acfc5d58e4c8ca0bb4920ab84613197
Alessia Bodini <44480802+alessiabodini@users.noreply.github.com>
2021-09-17 12:26:17 +0200

    Defined constructor for Results and MLEResults

--- include/results.hpp
+++ include/results.hpp
@@ -1,6 +1,57 @@
 #pragma once
 
+#include "latent_variables.hpp"
 #include "output/tableprinter.hpp"
 #include "tests/nhst.hpp"
 
-class Results {};
+#include <algorithm>
+
+class Results {
+protected:
+    std::vector<std::string> _X_names;
+    std::string _model_name;
+    std::string _model_type;
+    LatentVariables _z;
+    Eigen::VectorXd _z_values;
+    std::vector<double> _results; // FIXME: OptimizeResult type in Python (da scipy) ma viene utilizzato solo l'array
+                                  // non gli altri oggetti al suo interno
+    Eigen::MatrixXd _data;        ///< Predicted values for the time series and length-adjusted time series
+    int _index;                   // FIXME: Può essere anche una lista di indici
+    bool _multivariate_model;
+    std::function<double(Eigen::VectorXd)> _objective_object; ///< Likelihood or posterior
+    std::string _method;
+    uint8_t _z_hide;
+    int _max_lag;
+    Eigen::VectorXd _ihessian;
+    Eigen::VectorXd _signal;
+    // FIXME: _scores, _states e _states_var non sono mai usate con i modelli ARMA
+    Eigen::VectorXd _scores;
+    Eigen::VectorXd _states;
+    Eigen::VectorXd _states_var;
+    size_t _data_length;
+    std::string _data_name;
+    uint8_t _rounding_points;
+    double _aic;
+    double _bic;
+
+    Results(std::vector<std::string> data_name, std::vector<std::string> X_names, std::string model_name,
+            const std::string& model_type, const LatentVariables& latent_variables, std::vector<double> results,
+            Eigen::MatrixXd data, int index, bool multivariate_model,
+            std::function<double(Eigen::VectorXd)> objective_object, std::string method, bool z_hide, int max_lag,
+            Eigen::VectorXd ihessian = Eigen::VectorXd::Zero(0), Eigen::VectorXd scores = Eigen::VectorXd::Zero(0),
+            Eigen::VectorXd states = Eigen::VectorXd::Zero(0), Eigen::VectorXd states_var = Eigen::VectorXd::Zero(0));
+};
+
+class MLEResults : Results {
+private:
+    double _loglik;
+
+public:
+    MLEResults(std::vector<std::string> data_name, std::vector<std::string> X_names, std::string model_name,
+               const std::string& model_type, LatentVariables latent_variables, std::vector<double> results,
+               Eigen::MatrixXd data, int index, bool multivariate_model,
+               std::function<double(Eigen::VectorXd)> objective_object, std::string method, bool z_hide, int max_lag,
+               Eigen::VectorXd ihessian = Eigen::VectorXd::Zero(0), Eigen::VectorXd scores = Eigen::VectorXd::Zero(0),
+               Eigen::VectorXd states     = Eigen::VectorXd::Zero(0),
+               Eigen::VectorXd states_var = Eigen::VectorXd::Zero(0));
+};
--- src/results.cpp
+++ src/results.cpp
@@ -1,1 +1,63 @@
 #include "results.hpp"
+
+Results::Results(std::vector<std::string> data_name, std::vector<std::string> X_names, std::string model_name,
+                 const std::string& model_type, const LatentVariables& latent_variables, std::vector<double> results,
+                 Eigen::MatrixXd data, int index, bool multivariate_model,
+                 std::function<double(Eigen::VectorXd)> objective_object, std::string method, bool z_hide, int max_lag,
+                 Eigen::VectorXd ihessian, Eigen::VectorXd scores, Eigen::VectorXd states, Eigen::VectorXd states_var)
+    : _X_names{std::move(std::move(std::move(std::move(std::move(X_names)))))}, _model_name{std::move(
+                                                                                        std::move(model_name))},
+      _model_type{model_type}, _z{latent_variables}, _z_values{latent_variables.get_z_values()}, _results{std::move(
+                                                                                                         results)},
+      _data{std::move(std::move(data))}, _index{index}, _multivariate_model{multivariate_model},
+      _objective_object{std::move(objective_object)}, _method{std::move(method)}, _z_hide{static_cast<uint8_t>(z_hide)},
+      _max_lag{max_lag}, _ihessian{std::move(ihessian)}, _scores{std::move(scores)}, _states{std::move(states)},
+      _states_var{std::move(states_var)} {
+    if (_multivariate_model) {
+        _data_length = _data.row(0).size();
+    } else {
+        _data_length = _data.rows();
+    }
+
+    std::ostringstream oss;
+    std::copy(data_name.begin(), data_name.end(), std::ostream_iterator<std::string>(oss, ","));
+    _data_name = oss.str();
+
+    if (_model_type == "LLT" || model_type == "LLEV")
+        _rounding_points = 10;
+    else
+        _rounding_points = 4;
+}
+
+MLEResults::MLEResults(std::vector<std::string> data_name, std::vector<std::string> X_names, std::string model_name,
+                       const std::string& model_type, LatentVariables latent_variables, std::vector<double> results,
+                       Eigen::MatrixXd data, int index, bool multivariate_model,
+                       std::function<double(Eigen::VectorXd)> objective_object, std::string method, bool z_hide,
+                       int max_lag, Eigen::VectorXd ihessian, Eigen::VectorXd scores, Eigen::VectorXd states,
+                       Eigen::VectorXd states_var)
+    : Results{std::move(data_name),
+              X_names,
+              model_name,
+              model_type,
+              latent_variables,
+              results,
+              data,
+              index,
+              multivariate_model,
+              std::move(objective_object),
+              method,
+              z_hide,
+              max_lag,
+              ihessian,
+              scores,
+              states,
+              std::move(states_var)} {
+    if (_method == "MLE" || _method == "OLS") {
+        _loglik = -_objective_object(_z_values);
+        _aic    = 2 * _z_values.size() + 2 * _objective_object(_z_values);
+        _bic    = 2 * _objective_object(_z_values) + _z_values.size() + log(_data_length);
+    } else if (_method == "PML") {
+        _aic = 2 * _z_values.size() + 2 * _objective_object(_z_values);
+        _bic = 2 * _objective_object(_z_values) + _z_values.size() + log(_data_length);
+    }
+}
