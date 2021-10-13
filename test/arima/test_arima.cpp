02a162af553d794baa3b1eca264c49388418a4cb
e5ti <ettore.cinquetti@gmail.com>
2021-10-13 17:48:16 +0200

    expanded test arima

--- test/arima/test_arima.cpp
+++ test/arima/test_arima.cpp
@@ -43,6 +43,8 @@ TEST_CASE("Tests an ARIMA model with a Normal family", "[ARIMA]") {
         delete x;
     }
 
+    // @TODO: ARIMA with 2 AR 2 MA (check if previous two work)
+
     SECTION("Test prediction length", "[fit]") {
         ARIMA model{data, 2, 2};
         Results* x{model.fit()};
@@ -69,7 +71,7 @@ TEST_CASE("Tests an ARIMA model with a Normal family", "[ARIMA]") {
         for(auto& vec : test_df.data)
             for(auto& elem : vec)
                 REQUIRE(!std::isnan(elem));
-
+        delete x;
     }
 
     SECTION("Test that the predictions IS are not nans") {
@@ -80,8 +82,67 @@ TEST_CASE("Tests an ARIMA model with a Normal family", "[ARIMA]") {
         for(auto& vec : test_df.data)
             for(auto& elem : vec)
                 REQUIRE(!std::isnan(elem));
-
+        delete x;
     }
 
+    SECTION("Test predictions not having constant values"){
+        ARIMA model{data, 2, 2};
+        Results* x{model.fit()};
+        DataFrame predictions = model.predict(10, false);
+        REQUIRE(! (std::adjacent_find( predictions.data.begin(), predictions.data.end(), std::not_equal_to<>() )
+                  == predictions.data.end() ) );
+        delete x;
+    }
 
+    SECTION("Test predictions not having constant values"){
+        ARIMA model{data, 2, 2};
+        Results* x{model.fit()};
+        DataFrame predictions = model.predict_is(10, false);
+        REQUIRE(! (std::adjacent_find( predictions.data.begin(), predictions.data.end(), std::not_equal_to<>() )
+        == predictions.data.end() ) );
+        delete x;
+    }
+
+    SECTION("Tests prediction intervals are ordered correctly"){
+        ARIMA model{data, 2, 2};
+        Results* x{model.fit()};
+        DataFrame predictions = model.predict(10, false);
+
+        // forecasted_values, prediction_01, prediction_05, prediction_95, prediction_99
+        for(int i = 4; i > 1; i--) {
+            double sup_min_element = *min_element(std::begin(predictions.data.at(i)), std::end(predictions.data.at(i)));
+            double inf_max_element = *min_element(std::begin(predictions.data.at(i-1)), std::end(predictions.data.at(i-1)));
+            REQUIRE(sup_min_element >= inf_max_element);
+        }
+        delete x;
+    }
+
+    //@TODO: se giusto, fai tutti gli altri (fino a riga 161)
+
+    SECTION("Test sampling function") {
+        ARIMA model{data, 2, 2};
+        auto op = std::nullopt;
+        // ok cosa
+        std::optional<Eigen::MatrixXd>& op_matrix = (std::optional<Eigen::MatrixXd>&) std::nullopt;
+        Results* x{model.fit("BBVI", false, op_matrix, 100, op, op, op, op, op, op, op, true)};
+        Eigen::MatrixXd sample = model.sample(100);
+
+        REQUIRE(sample.rows() == 100);
+        REQUIRE(sample.cols() == data.size()-2);
+
+        delete x;
+    }
+
+    SECTION("Test ppc value"){
+        ARIMA model{data, 2, 2};
+        auto op = std::nullopt;
+        std::optional<Eigen::MatrixXd>& op_matrix = (std::optional<Eigen::MatrixXd>&) std::nullopt;
+        Results* x{model.fit("BBVI", false, op_matrix, 100, op, op, op, op, op, op, op, true)};
+        double p_value = model.ppc();
+
+        REQUIRE(p_value >= 0.0);
+        REQUIRE(p_value <= 1.0);
+
+        delete x;
+    }
 }
